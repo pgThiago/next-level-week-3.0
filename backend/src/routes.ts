6951514bc5ff101179afd4c1fc601995807b847c
Thiago Silva <thiago25pro@gmail.com>
2020-10-14 16:03:41 -0300

    backend day 2

---
+++ backend/src/routes.ts
@@ -0,0 +1,15 @@
+import Router from 'express';
+import OrphanagesController from './controllers/OrphanagesController';
+
+import multer from 'multer';
+import uploadConfig from './config/upload';
+
+
+const routes = Router();
+const upload = multer(uploadConfig);
+
+routes.get('/orphanages', OrphanagesController.index);
+routes.get('/orphanage/:id', OrphanagesController.show);
+routes.post('/orphanages', upload.array('images'), OrphanagesController.create);
+
+export default routes;
